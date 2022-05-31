4799f7231e3f4fed99ed0584b436ad739bef711f
clayton <6334+clayton@users.noreply.github.com>
2022-05-31 12:24:00 -0700

    protect dashboard controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,4 @@
-class DashboardController < ApplicationController
+class DashboardController < AuthenticatedUserController
   def show
     @tweets_in_30_days = current_user.topics.map(&:tweets).flatten.select { |t| t.created_at > 30.days.ago }.size
     @twitter_search_results_in_30_days = current_user.topics.map(&:twitter_search_results).flatten.select { |t| t.created_at > 30.days.ago }.size
