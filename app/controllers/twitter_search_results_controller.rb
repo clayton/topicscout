44e6c4e3a98db3038445d89b465d295453db3c2d
clayton <6334+clayton@users.noreply.github.com>
2023-01-22 16:45:21 -0700

    adds search now functionality

--- app/controllers/twitter_search_results_controller.rb
+++ app/controllers/twitter_search_results_controller.rb
@@ -1,6 +1,6 @@
 class TwitterSearchResultsController < ApplicationController
   def create
-    @topic = Topic.find_by(params[:topic_Id])
+    @topic = Topic.find_by(id: params[:topic_id])
     @topic.twitter_search_results.create
 
     redirect_to topic_tweets_url(@topic)
