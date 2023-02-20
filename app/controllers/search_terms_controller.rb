f3d6ed2f0ff5ee791fe353ef717e34fd1af4a5cc
clayton <6334+clayton@users.noreply.github.com>
2023-02-20 13:56:52 -0700

    sends user back to correct page on archive or save

--- app/controllers/search_terms_controller.rb
+++ app/controllers/search_terms_controller.rb
@@ -1,7 +1,7 @@
 class SearchTermsController < AuthenticatedUserController
   def create
     @topic = Topic.find(params[:topic_id])
-    @search_term = @topic.search_terms.create(search_term_params)
+    @search_term = @topic.search_terms.find_or_create_by(search_term_params)
 
     redirect_to topic_saves_path(@topic)
   end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -7,12 +7,12 @@ class TweetsController < AuthenticatedUserController
   def update
     @topic = Topic.find_by(id: params[:topic_id])
     @tweet = @topic.tweets.find_by(id: params[:id])
-    @tweet.update!(tweet_params)
+    @tweet.update!(tweet_params.except(:page))
 
-    redirect_to topic_tweets_path(@topic)
+    redirect_to topic_tweets_path(@topic, page: tweet_params[:page])
   end
 
   def tweet_params
-    params.require(:tweet).permit(:ignored, :saved, :archived)
+    params.require(:tweet).permit(:ignored, :saved, :archived, :page)
   end
 end
