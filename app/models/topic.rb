d6915f26fd150957277950859a8c884f3060dba3
clayton <6334+clayton@users.noreply.github.com>
2023-03-01 10:21:52 -0700

    reject blank terms and list ids

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -15,9 +15,9 @@ class Topic < ApplicationRecord
   after_create_commit :initial_search
   after_create_commit :create_list_on_twitter
 
-  accepts_nested_attributes_for :search_terms, allow_destroy: true
-  accepts_nested_attributes_for :negative_search_terms, allow_destroy: true
-  accepts_nested_attributes_for :twitter_lists, allow_destroy: true
+  accepts_nested_attributes_for :search_terms, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :negative_search_terms, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :twitter_lists, allow_destroy: true, reject_if: :all_blank
 
   def user_auth_token
     user.auth_token
