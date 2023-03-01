f0d7906d2aeeb3989de19f7d05b9d82ab0c2265d
clayton <6334+clayton@users.noreply.github.com>
2023-03-01 13:56:38 -0700

    raise some errors if things are missing when adding user to list

--- app/jobs/promote_user_to_list_job.rb
+++ app/jobs/promote_user_to_list_job.rb
@@ -2,9 +2,14 @@ class PromoteUserToListJob < ApplicationJob
   queue_as :priority
 
   def perform(token, list_id, user_id)
+    
     client = Birder::Client.new(token)
 
     begin
+      raise "Missing Token", "Token is missing" if token.blank?
+      raise "Missing List ID", "List ID is missing" if list_id.blank?
+      raise "Missing User ID", "User ID is missing" if user_id.blank?
+      
       client.list.promote(list_id, user_id)
     rescue Birder::Error => e
       Honeybadger.notify(e)
