f0de5d12ee1b87072116ce3cfba44ac078dd3dc8
clayton <6334+clayton@users.noreply.github.com>
2023-03-05 15:49:52 -0700

    fix archiving

--- app/controllers/archived_tweets_controller.rb
+++ app/controllers/archived_tweets_controller.rb
@@ -1,6 +1,17 @@
 class ArchivedTweetsController < ApplicationController
   include ActionView::RecordIdentifier
 
+  def create
+    @topic = Topic.find(params[:topic_id])
+    permitted_params = params.permit(:page, :sort, :time_filter, :visibility_filter, :topic_id)
+    @tweets = @topic.unedited_tweets(permitted_params[:sort], permitted_params[:time_filter],
+                                     permitted_params[:visibility_filter])
+
+    @tweets.update_all(archived: true)
+
+    redirect_to topic_tweets_path(@topic)
+  end
+
   def update
     @topic = Topic.find(params[:topic_id])
     @tweet = @topic.tweets.find(params[:id])
--- app/controllers/archived_urls_controller.rb
+++ app/controllers/archived_urls_controller.rb
@@ -1,5 +1,16 @@
 class ArchivedUrlsController < AuthenticatedUserController
   include ActionView::RecordIdentifier
+
+  def create
+    @topic = Topic.find(params[:topic_id])
+    permitted_params = params.permit(:page, :sort, :time_filter, :visibility_filter, :topic_id)
+    @urls = @topic.unedited_urls(permitted_params[:sort], permitted_params[:time_filter],
+                                     permitted_params[:visibility_filter])
+
+    @urls.update_all(archived: true)
+
+    redirect_to topic_urls_path(@topic)
+  end
   
   def update
     @topic = Topic.find(params[:topic_id])
